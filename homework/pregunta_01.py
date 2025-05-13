f73bd359037f6f09ede1e89dbfc73fea230be39b
alariaso <141962262+alariaso@users.noreply.github.com>
2025-05-12 19:50:10 -0500

    a quien se le ocurrio que era buena idea ese formato

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,8 +1,27 @@
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
+import pandas as pd
 
 # pylint: disable=import-outside-toplevel
+
+
+def clean_header(header):
+    return header.lower().replace(" ", "_")
+
+
+def clean_word(word):
+    return ' '.join(filter(lambda x: x != '', word.split(" ")))
+
+
+def clean_words(words_str):
+    return ', '.join(map(clean_word, words_str.split(",")))
+
+
+def parse_percentage(percentage: str):
+    s = percentage.removesuffix(" %")
+    s = s.replace(",", ".")
+    return float(s)
 
 
 def pregunta_01():
@@ -18,3 +37,58 @@ def pregunta_01():
 
 
     """
+    widths = [9, 16, 16]
+    headers = [[], [], [], []]
+    rows = []
+
+
+    with open("files/input/clusters_report.txt") as f:
+        lines = iter(f.readlines())
+
+        # Obtener la cabecera
+        for _ in range(2):
+            line = next(lines).rstrip()
+            i = 0
+            j = 0
+            for width in widths:
+                headers[j].append(line[i:i+width].strip())
+                j+=1
+                i+=width
+            headers[j].append(line[i:].strip())
+        headers = list(map(lambda x: ' '.join(x).strip(), headers))
+
+        # ignorar linea vacía
+        next(lines)
+
+        # ignorar separador
+        next(lines)
+
+        # Obtener las filas
+        while (line := next(lines, None)) is not None:
+            i = 0
+            j = 0
+            row = []
+            for width in widths:
+                row.append(line[i:i+width].strip())
+                j += 1
+                i+=width
+            row.append(line[i:].strip())
+
+            row[-1] = clean_words(row[-1]).strip()
+
+            if row[0] == '':
+                if row[-1] == '':
+                    continue
+                rows[-1][-1] += " " + row[-1]
+            else:
+                row[0] = int(row[0])
+                row[1] = int(row[1])
+                row[2] = parse_percentage(row[2])
+                rows.append(row)
+
+    headers = list(map(clean_header, headers))
+
+    for row in rows:
+        row[-1] = row[-1].removesuffix(".")
+
+    return pd.DataFrame(rows, columns=headers)
